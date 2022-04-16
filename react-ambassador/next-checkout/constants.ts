9e7fbf0c848fe8a1224ccaf9b95afdc60b6233bb
Krongkaew Ponboobpha <krongkaew@enres.co>
2022-04-17 01:51:12 +0700

    remove cached react-ambassador

---
+++ react-ambassador/next-checkout/constants.ts
@@ -0,0 +1,5 @@
+export default {
+  endpoint: "http://localhost:8000/api/checkout",
+  stripe_key:
+    "pk_test_51KmFAYLI2KMaR4vpHFl2sS6EmbDMeJn31c1wh3VglRHlsvFe6nM0X7Y3gePKXBWcPVvVwmcL9ZLoLEqOww0fQPP9004AlZPikh",
+};
