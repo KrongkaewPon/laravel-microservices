6ed602233737d74f66cb668c545e4c6762d9e26d
Krongkaew Ponboobpha <krongkaew@enres.co>
2022-04-21 00:58:47 +0700

    sec7 - . Importing Data

--- admin/database/migrations/2021_02_20_091057_create_links_table.php
+++ admin/database/migrations/2021_02_20_091057_create_links_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateLinksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->tinyInteger('is_admin')->default(0);
+        Schema::create('links', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+            $table->string('code')->unique();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('links');
     }
 }
