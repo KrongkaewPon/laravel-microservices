a6a91af32fd9b9527a49fbf84e08557e1cf5e4e2
Krongkaew Ponboobpha <krongkaew@enres.co>
2022-04-20 10:56:03 +0700

    sec5 - Moving Controllers, Configuring Kafka

--- checkout/routes/api.php
+++ checkout/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Checkout
+Route::prefix('checkout')->group(function () {
+    Route::get('links/{code}', [LinkController::class, 'show']);
+    Route::post('orders', [OrderController::class, 'store']);
+    Route::post('orders/confirm', [OrderController::class, 'confirm']);
 });
